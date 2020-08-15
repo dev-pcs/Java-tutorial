bfdfeff81b16f824bcb066b93f8d8fdc99a9d52f
Priyank Shah <priyankshah@MacBook-Pro.attlocal.net>
2020-08-15 17:31:11 -0500

    Hello world executed

--- HelloWorld/src/com/company/Main.java
+++ HelloWorld/src/com/company/Main.java
@@ -1,8 +1,8 @@
-package com.company;
+package com.company;        //main class belongs to this package
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) {        //main method should always be static, void- not returning anything
+        System.out.println("Hello World");
     }
 }
