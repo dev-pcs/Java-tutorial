bc979c7c92296ef648f41b4636fab05e606cf452
Priyank Shah <priyankshah@MacBook-Pro.attlocal.net>
2020-08-16 21:30:00 -0500

    new project

---
+++ OOPM/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var textBox1 = new TextBox();
+        textBox1.setText("Box1");
+        System.out.println(textBox1.text);
+    }
+}
